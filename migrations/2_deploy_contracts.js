da95312dfeb74f78fd81f93e684bfe178d9555b9
Long Hoang <long@mindworker.de>
2017-12-20 00:31:15 +0800

    Fix live migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,17 @@
 var Community = artifacts.require("./Community.sol");
 
 module.exports = function(deployer) {
+  let con;
   deployer.then(() => {
     return deployer.deploy(Community, 10);
   }).then(() => {
     return Community.deployed();
-  })
+  }).then(instance => {
+    con = instance;
+    return con.deposit({value: 300000});
+  }).then(txn => {
+    return con.createProposal(100000);
+  }).then(txn => {
+    return con.createProposal(50000);
+  });
 };
